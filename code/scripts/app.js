aa9b210a5dcfbd1f47b63e9ff79c741265dbef58
yashbavishi <ybavishi@bu.edu>
2017-10-28 18:26:54 -0400

    Adding angular contoller

---
+++ code/scripts/app.js
@@ -0,0 +1,7 @@
+'use strict';
+
+
+// Define an angular module for the application
+
+var app = angular.module('ukubukaFinance', ['ngRoute',
+    'ngCookies'])
---
+++ code/scripts/currencyConverter.js
@@ -0,0 +1,40 @@
+app.controller('currencyConverter', function($scope, $http, $window, $location, $anchorScroll) {
+  $scope.form = {};
+  $scope.masterform = {};
+  $scope.currencies = [ {name: 'Rupee', value: "RUP"}, {name: 'BITCNY', value: 'BITCNY'}, {name: 'Bitcoin', value: 'BIT'}];
+  $scope.returnPath = '';
+  $scope.myVar = false;
+
+
+$scope.gotoTop = function() {
+      // set the location.hash to the id of
+      // the element you wish to scroll to.
+      $location.hash('top');
+
+      // call $anchorScroll()
+      $anchorScroll();
+    };
+
+$scope.getChart =  function(form) {
+  angular.copy($scope.form, $scope.masterform);  
+  $scope.myVar = true;
+  var data = {
+        "from": $scope.form.from,
+        'to': $scope.form.to    
+    };
+  
+  $http.post('ukubukafinance:6090/api/v1/data/fx', data
+   
+  ).then(function (response) {
+    
+    $scope.returnPath = response.data;
+     
+   });
+   //$scope.getAllRecords(); 
+   $scope.isDisabled = false;
+   $scope.selectDefaults();
+}
+
+
+
+});
